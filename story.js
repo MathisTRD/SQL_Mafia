2199b01112bd4b990281ada28e5c07d27b6f7f4b
Mathis <mathishecktr@gmail.com>
2024-03-24 17:27:46 +0100

    Update story.js

--- story.js
+++ story.js
@@ -1,8 +1,8 @@
 export default [
   {
     text: [
-      "Nachdem Salvatore Martino über 12 Jahre wegen eines bewaffneten Überfalls auf einen Politiker im Gefängnis saßt, ist heute der Tag, an welchem du deine Freiheit wiedererlagnst. Du gehst auf die Straße und bewunderst, wie sehr sich die Stadt seit deiner Verhaftung verändert hat. Dein treuster Untertante aus deiner alten Gang kommt dich abholen und hilft dir, dich zurecht zu finden.",
-      "Angelo: Wow Boss, du bist jetzt also auch mal aus'm Bau. Hat ja ewig gedauert! Du glaubst gar nicht, was hier bei uns alles passiert ist. Es haben haben sich echt viele neue Gangs gebildet und die Konkurrenz ist hart! ",
+      "Nachdem Salvatore Martino über 12 Jahre wegen eines bewaffneten Überfalls auf einen Politiker im Gefängnis saß, ist heute der Tag, an welchem du deine Freiheit wiedererlangst. Du gehst auf die Straße und bewunderst, wie sehr sich die Stadt seit deiner Verhaftung verändert hat. Dein treuster Untertante aus deiner alten Gang kommt dich abholen und hilft dir, dich zurecht zu finden.",
+      "Angelo: Wow Boss, du bist jetzt also auch mal aus'm Bau. Hat ja ewig gedauert! Du glaubst gar nicht, was hier bei uns alles passiert ist. Es haben haben sich echt viele neue Gangs gebildet und die Konkurrenz ist hart!",
     ],
     task: `Finde heraus, welche Gruppen es alles gibt.`,
     sqlRequest: `SELECT * FROM Gruppen`,
